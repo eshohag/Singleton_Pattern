2a5628f2403e5ea55fc9b45756f65c231eecdde1
eshohag <eshohag@outlook.com>
2024-11-18 20:20:22 +0600

    Add project files.

---
+++ Singleton_Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the only object available
+            SingleObject ob = SingleObject.GetInstance();
+
+            //show the message
+            ob.ShowMessage();
+            ob.ShowMessage();
+            ob = SingleObject.GetInstance();
+            ob.ShowMessage();
+            ob = SingleObject.GetInstance();
+            ob.ShowMessage();
+
+            //If we create instance multiple times then shows the same value, it's called singleton. 
+        }
+    }
+}
---
+++ Singleton_Pattern/SingleObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public class SingleObject
+    {
+        //create an object of SingleObject
+        private static SingleObject instance = new SingleObject();
+
+        //Get the only object available
+        public static SingleObject GetInstance()
+        {
+            return instance;
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello World!-" + DateTime.Now.ToString());
+        }
+    }
+}
